3bae7d838d95800cd0fcdd51d275626c28d095a7
Mayankt-21 <92510723+Mayankt-21@users.noreply.github.com>
2024-12-30 12:05:07 +0530

    .env hide + db organized

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,16 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 // Load environment variables
 dotenv.config();
+
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDB connected successfully!");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
 
 // Basic route
 app.get("/", (req, res) => {
